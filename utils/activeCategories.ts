4cebdfaca4472b2f1f3c49bd71d5c0fadbf20482
ibrahim <ibrahim@softzino.com>
2024-06-27 23:53:15 +0600

    tried to solve scroll navigation issues

--- utils/activeCategories.ts
+++ utils/activeCategories.ts
@@ -10,8 +10,9 @@ const useSectionHighlight = () => {
 
       const sections = document.querySelectorAll<HTMLElement>('section');
       const navItems = document.querySelectorAll<HTMLLIElement>('nav li');
+      const nav = document.querySelector<HTMLUListElement>('nav');
 
-      console.log("hello bokku...", navItems, "here is section   ", sections);
+      console.log("hello bokku...", navItems.length, "nav lentgth ", nav?.clientWidth);
 
       if (scrollPosition >= 100) {
         sections.forEach((section, index) => {
@@ -21,7 +22,6 @@ const useSectionHighlight = () => {
             navItems.forEach(item => item.classList.remove('active'));
             if (navItems[index]) {
               navItems[index].classList.add('active');
-              navItems[index].scrollIntoView({ behavior: "smooth", inline: "center" });
 
             }
           }
@@ -30,38 +30,9 @@ const useSectionHighlight = () => {
         navItems.forEach(item => item.classList.remove('active'));
         if (navItems[0]) {
           navItems[0].classList.add('active');
-          navItems[0].scrollIntoView({ behavior: "smooth", inline: "center" });
         }
       }
     };
-
-     // Function to handle the click event on nav links
-     const handleClick = (event: MouseEvent) => {
-      const target = event.target as HTMLElement;
-      if (target.tagName === 'A') {
-        event.preventDefault(); // Prevent default link behavior
-
-        const navLinks = document.querySelectorAll<HTMLAnchorElement>('nav a');
-        navLinks.forEach(link => link.classList.remove('active'));
-
-        target.classList.add('active');
-        target.scrollIntoView({ behavior: "smooth", inline: "center" });
-
-        // Smoothly scroll to the corresponding section
-        const sectionId = target.getAttribute('href')?.substring(1);
-        const section = document.getElementById(sectionId ?? '');
-        if (section) {
-          section.scrollIntoView({ behavior: "smooth" });
-        }
-      }
-    };
-
-    // Add event listeners
-    const navLinks = document.querySelectorAll<HTMLAnchorElement>('nav a');
-    navLinks.forEach(link => {
-      link.addEventListener('click', handleClick);
-    });
-
 
     window.addEventListener('scroll', handleScroll);
 
@@ -71,9 +42,7 @@ const useSectionHighlight = () => {
     // Cleanup event listener on component unmount
     return () => {
       window.removeEventListener('scroll', handleScroll);
-      navLinks.forEach(link => {
-        link.removeEventListener('click', handleClick);
-      });
+     
     };
   }, []);
 };
